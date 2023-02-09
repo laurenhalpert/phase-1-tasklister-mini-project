3c4709adceb054fd9a2f4084b12e4ad7e1acf249
Lauren <halpertln@gmail.com>
2023-02-09 13:10:31 -0700

    complete lab

--- src/index.js
+++ src/index.js
@@ -1,3 +1,22 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+  let form = document.querySelector("form");
+  form.addEventListener("submit", (e) => {
+    e.preventDefault();
+    buildToDo(e.target["new-task-description"].value)
+    form.reset();
+  })
 });
+
+function buildToDo (todo) {
+  let li = document.createElement("li");
+  let btn = document.createElement("button");
+  btn.textContent= "x";
+  btn.addEventListener("click", handleDelete);
+  li.textContent = `${todo} `;
+  li.appendChild(btn);
+  document.querySelector("#tasks").appendChild(li);
+}
+
+function handleDelete(e) {
+  e.target.parentNode.remove();
+}
